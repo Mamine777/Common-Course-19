f361a8ebf4eedb706a42138505ffb3e95b1adb79
Mohamed Amine Kariouh <mokariou@fu-r7-p2.s19.be>
2024-10-14 17:54:47 +0200

    last fix

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: mokariou <mokariou@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/10 18:15:46 by mokariou          #+#    #+#             */
-/*   Updated: 2024/10/14 17:37:14 by mokariou         ###   ########.fr       */
+/*   Updated: 2024/10/14 17:54:06 by mokariou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ void	*ft_calloc(size_t num_elements, size_t size_of_ele)
 	void	*res;
 	size_t	total;
 
-	if (num_elements != 0 && SIZE_MAX / num_elements < size_of_ele)
-		return (NULL);
 	total = num_elements * size_of_ele;
 	if (total == 0)
 		return (malloc(1));
@@ -32,12 +30,12 @@ void	*ft_calloc(size_t num_elements, size_t size_of_ele)
 // #include <stdio.h>
 // int	main(void)
 // {
-// 	int	*tab;
+// 	size_t	*tab;
 // 	size_t	count = 6;
 
 // 	tab = ft_calloc(SIZE_MAX, sizeof(SIZE_MAX));
 
 // 	for (size_t i = 0; i < count; i++){
-// 		printf("%d", tab[i]);
+// 		printf("%zu", tab[i]);
 // 	}
 // }
